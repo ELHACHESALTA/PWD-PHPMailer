ad56624d56fa33f9700358e615f43d119b4d848e
guillermoagueronqn <guillermoagueronqn@gmail.com>
2023-10-03 22:34:10 -0300

    Se deja funcionando el 2do test, y se agregó un archivo para la prueba de dicho test

--- test/test_implementacionAdjunto.php
+++ test/test_implementacionAdjunto.php
@@ -1,2 +1,38 @@
 <?php
+    use PHPMailer\PHPMailer\PHPMailer;
+
+    include_once('../util/class/PHPMailer.php');
+    include_once('../util/class/SMTP.php');
+
+    $mail = new PHPMailer(true);
+    //Server settings
+    $mail->SMTPDebug = 0;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                       //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'reclamoventas@gmail.com';                                     //correo
+    $mail->Password   = 'aveevgtlmqjcspbf';                                     //contraseña
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('reclamoventas@gmail.com', 'nombre'); // usuario que envía
+    $mail->addAddress('reclamoventas@gmail.com');     // mail que recibe 
+
+    //Attachments
+    // esto se utiliza para enviar archivos
+    $mail->addAttachment("../archivos/tpLibrerias.pdf");    // archivo a enviar
+    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Asunto de la prueba del correo enviado';
+    $mail->Body    = 'Este es el cuerpo del mail enviado para probar el PHPMailer';
+    // mensaje alternativo
+    //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    if($mail->send()){
+        echo 'El mensaje se ha enviado con exito!';
+    } else {
+        echo "El mensaje no se ha podido enviar. El error es: {$mail->ErrorInfo}";
+    }
 ?>
